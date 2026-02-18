d13035750bfe8f2e2f4b9796374d5687d18dde2f
jahidsharuar2021-prog <jahidsharuar2021@gmail.com>
2026-02-19 01:12:48 +0600

    add css style in js

---
+++ js/cssin.js
@@ -0,0 +1,16 @@
+const sections=document.getElementsByTagName('section')
+// console.log(sections)
+// for(const section of sections){
+//     console.log(section);
+//     section.style.backgroundColor = 'lightblue';
+//     section.style.border='3px solid green';
+//  section.style.marginBottom ='3px';
+//  section.style.borderRadius ='15px';
+//  section .style.padding ='15px';
+
+// }
+
+for(const section of sections){
+
+    section.classList.add('section-card')
+}
--- js/hello.js
+++
@@ -1,3 +0,0 @@
-
-// you have to understand document is a one kind of object 
-console.log(document);
